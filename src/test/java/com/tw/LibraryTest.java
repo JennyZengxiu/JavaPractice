1436ab2bebea08d290ed76417ce9813fb5b661ce
zx <1374040664@qq.com>
2018-06-02 19:44:04 +0800

    add test

--- src/test/java/com/tw/LibraryTest.java
+++ src/test/java/com/tw/LibraryTest.java
@@ -4,29 +4,24 @@ import org.junit.Test;
 
 import java.util.LinkedList;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class LibraryTest {
     @Test
-    public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    public void should_get_true_when_student_format_is_correct() {
+        Library inputTest = new Library();
+        String studentInfo = "张三，120161，数学：75，语文：95，英语：80";
+        assertTrue("someLibraryMethod should return 'true'", inputTest.isStudentInfo(studentInfo));
     }
 
     @Test
-    public void testMockClass() throws Exception {
-        // you can mock concrete classes, not only interfaces
-        LinkedList mockedList = mock(LinkedList.class);
-
-        // stubbing appears before the actual execution
-        String value = "first";
-        when(mockedList.get(0)).thenReturn(value);
-
-        assertEquals(mockedList.get(0), value);
-
+    public void should_get_true_when_student_number_format_is_correct() {
+        Library inputTest = new Library();
+        String studentNum = "120123,123456";
+        assertTrue("someLibraryMethod should return 'true'", inputTest.isStudentNum(studentNum));
     }
 
 }
